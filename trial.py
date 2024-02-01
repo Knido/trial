029f612b640deee7cb71b8359fdc7926b07d05c8
Knido <knidobla@gmail.com>
2024-02-01 08:43:53 -0400

    merge trial

--- trial.py
+++ trial.py
@@ -3,5 +3,12 @@ name = "Selorm Siabi"
 # String - Slicing
 # [start:stop:step]
 
-output = name[::-1]
-print(output)
+# output = name[::-1]
+# print(output)
+
+def reverse(n):
+    reversed = n[::-1]
+    return reversed
+
+output2 = reverse(name)
+print(output2)
